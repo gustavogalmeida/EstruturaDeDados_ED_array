c03992c4193a360c7d7cef9a4af339b754f1a8fa
Gustavo Almeida <gustavogalvesalmeida@gmail.com>
2024-01-18 22:23:33 -0300

    created array and utilized builder

---
+++ src/Array/Array.java
@@ -0,0 +1,8 @@
+package Array;
+
+public class Array {
+    private String[] elements;
+    public Array(int size){
+        this.elements = new String[size];
+    }
+}
--- src/Array/Main.java
+++ src/Array/Main.java
@@ -1,5 +1,8 @@
+package Array;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Array array = new Array(5);
+        int i = 0;
     }
 }
