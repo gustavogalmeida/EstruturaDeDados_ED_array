f634f08cc085e3e82fd6f3677e436622dbb5e250
Gustavo Almeida <gustavogalvesalmeida@gmail.com>
2024-01-20 09:41:00 -0300

    Version two created for resolve problems with size of the array

--- src/Array/Array.java
+++ src/Array/Array.java
@@ -2,15 +2,27 @@ package Array;
 
 public class Array {
     private String[] elements;
-    public Array(int size){
-        this.elements = new String[size];
+    private int size;
+    public Array(int length){
+        this.elements = new String[length];
     }
-    public void add(String element){
+    public void addV1(String element){
         for (int i = 0; i <= this.elements.length; i++){
             if (elements[i] == null) {
                 elements[i] = element;
                 break;
             }
         }
+    }
+    public void addV2 (String element) throws Exception{
+        if (this.size < this.elements.length){
+            this.elements[this.size] = element;
+            this.size++;
+        } else {
+            throw new Exception("The Array is full. Cannot add new elements");
+        }
+    }
+    public void setSize(int size) {
+        this.size = size;
     }
 }
--- src/Array/Main.java
+++ src/Array/Main.java
@@ -2,8 +2,14 @@ package Array;
 
 public class Main {
     public static void main(String[] args) {
-        Array array = new Array(5);
-        array.add("element 1");
-        array.add("element 2");
+        Array array = new Array(2);
+        array.setSize(0);
+
+        try {
+            array.addV2("element 1");
+            array.addV2("element 2");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
