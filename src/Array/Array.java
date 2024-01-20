43192250138bf1dec3e1bca27904e0f53ffe1ec6
Gustavo Almeida <gustavogalvesalmeida@gmail.com>
2024-01-20 09:26:00 -0300

    method to add is working by iterating the entire array

--- src/Array/Array.java
+++ src/Array/Array.java
@@ -5,4 +5,12 @@ public class Array {
     public Array(int size){
         this.elements = new String[size];
     }
+    public void add(String element){
+        for (int i = 0; i <= this.elements.length; i++){
+            if (elements[i] == null) {
+                elements[i] = element;
+                break;
+            }
+        }
+    }
 }
--- src/Array/Main.java
+++ src/Array/Main.java
@@ -3,6 +3,7 @@ package Array;
 public class Main {
     public static void main(String[] args) {
         Array array = new Array(5);
-        int i = 0;
+        array.add("element 1");
+        array.add("element 2");
     }
 }
